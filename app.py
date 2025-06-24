87a4c9364a90b58c851ebc35def1567a9912c799
TuNombreDeUsuario <jordyyymendoza@gmail.com>
2025-06-24 11:11:04 -0600

    Intento de despliegue parte 3

--- app.py
+++ app.py
@@ -66,4 +66,6 @@ def query():
     })
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host="0.0.0.0", port=port)
+# app.run(port=5000, debug=True)
