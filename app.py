d05fa37d8ada91ffeb3e6872e9466930988c7334
TuNombreDeUsuario <jordyyymendoza@gmail.com>
2025-06-24 12:40:23 -0600

    Intento de despliegue parte 7

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def query():
         "answer": respuesta
     })
 
-# === 6. Inicializar server en puerto correcto ===
+# === 6. Inicializar server en puerto correctos ===
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
